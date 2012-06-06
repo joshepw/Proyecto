410d7b1706a25de46c407be384cec33cb6b71440
joshepw <joshepw@gmail.com>
2012-06-06 13:29:13 -0600

    Cambio de Branch y depuracion de Comentarios

--- src/proyecto/Position.java
+++ src/proyecto/Position.java
@@ -1,13 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyecto;
 
-/**
- *
- * @author joxhe
- */
 class Position {
     public int x;
     public int y;
--- src/proyecto/piezaTipo.java
+++ src/proyecto/piezaTipo.java
@@ -1,13 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyecto;
 
-/**
- *
- * @author joxhe
- */
 public enum piezaTipo {
     ZOMBIE,WOLF,VAMPIRE,DEATH;
 }
